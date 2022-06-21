c6bc8ca12c2f1a3fcb3dee145f81e8805279ade0
Mutlaq Aldhbuiub <matulkn@gmail.com>
2022-06-21 09:25:05 +0300

    Adjust the form to work on light & dark modes

---
+++ _nuxt/static/1655792604/en/thanks/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655792604",layout:"default",error:a,serverRendered:b,routePath:"\u002Fen\u002Fthanks",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f8f53a87"}},__i18n:{langs:{}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
